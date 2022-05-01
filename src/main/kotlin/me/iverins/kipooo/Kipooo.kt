5b13f5daef6065a742d437a9c2f86611e31eb2c9
Iveirns <iverins@protonmail.com>
2022-05-01 10:28:25 +0800

    Initial commit

---
+++ src/main/kotlin/me/iverins/kipooo/Kipooo.kt
@@ -0,0 +1,11 @@
+package me.iverins.kipooo
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object Kipooo : Plugin() {
+
+    override fun onLoad() {
+
+    }
+}
